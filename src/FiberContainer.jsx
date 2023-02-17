46ccfbcc8d4741e08b2a64237ca91fcab4cd9e78
dshot92 <dshot92@gmail.com>
2023-02-17 22:18:35 +0100

    arrow

--- src/FiberContainer.jsx
+++ src/FiberContainer.jsx
@@ -1,9 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import { CameraControls, Sky } from "@react-three/drei";
 import { Scene } from "./Scene";
+import Grid from "./Grid";
 import { Suspense } from "react";
 import { BeatLoader } from "react-spinners";
 import { React, forwardRef } from "react";
+import ReactDOM from "react-dom";
 
 function Loading() {
     return (
@@ -14,8 +16,16 @@ function Loading() {
 }
 
 export const FiberContainer = forwardRef((props, ref) => {
+    const handleCardClick = (path) => {
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(<Grid />);
+    };
     return (
         <Suspense fallback={<Loading />}>
+            <div className={"back"}>
+                {/* <span onClick={handleCardClick}>back</span> */}
+                <span onClick={handleCardClick}>&#x2190;</span>
+            </div>
             <Canvas dpr={3} camera={{ position: [7500, 15000, 7500], fov: 50, near: 10, far: 10000000 }}>
                 <Scene model={props.path} />
                 {/* <Sky distance={450000} sunPosition={[0, 1000, 0]} inclination={0} azimuth={0.25} /> */}
