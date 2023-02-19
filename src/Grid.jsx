b30975032642c8813ffbf12c3e0b4fb8a24ae2e7
dshot92 <dshot92@gmail.com>
2023-02-19 03:11:50 +0100

    Dynamic load not working on server side

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -4,30 +4,37 @@ import { FiberContainer } from "./FiberContainer";
 import { createRoot } from "react-dom/client";
 import axios from "axios";
 
-const MODEL_FOLDER = "./models/";
-const FILE_EXTENSION = ".fbx";
+// const MODEL_FOLDER = "./models/";
+// const FILE_EXTENSION = ".fbx";
 
 const Grid = () => {
-	const [items, setFiles] = useState([]);
+	// const [items, setFiles] = useState([]);
 
-	useEffect(() => {
-		axios
-			.get(MODEL_FOLDER)
-			.then((response) => {
-				const fbxFiles = response.data
-					.filter((file) => file && file.endsWith && file.endsWith(FILE_EXTENSION))
-					// .sort((a, b) => new Date(a.modifiedDate) - new Date(b.modifiedDate))
-					.map((file, index) => ({
-						id: index + 1,
-						path: `${MODEL_FOLDER}/${file}`,
-						name: file,
-					}));
-				setFiles(fbxFiles);
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	}, []);
+	// useEffect(() => {
+	// 	axios
+	// 		.get(MODEL_FOLDER)
+	// 		.then((response) => {
+	// 			const fbxFiles = response.data
+	// 				.filter((file) => file && file.endsWith && file.endsWith(FILE_EXTENSION))
+	// 				.map((file, index) => ({
+	// 					id: index + 1,
+	// 					path: `${MODEL_FOLDER}/${file}`,
+	// 					name: file,
+	// 				}));
+	// 			setFiles(fbxFiles);
+	// 		})
+	// 		.catch((error) => {
+	// 			console.error(error);
+	// 		});
+	// }, []);
+
+	const items = [
+		{ id: 1, title: "Elephant Tower Raw", path: "./models/Elephant_Tower.fbx" },
+		{ id: 2, title: "Elephant Tower Remesh", path: "./models/Elephant_Tower_Remesh.fbx" },
+		{ id: 3, title: "Big Ben Solid", path: "./models/Big_Ben_Solid.fbx" },
+		{ id: 4, title: "T Hotel Solid", path: "./models/T_Hotel.fbx" },
+		{ id: 4, title: "Arc de Triomphe", path: "./models/Arc_de_Triomphe.fbx" },
+	];
 
 	const handleCardClick = (path) => {
 		const rootElement = document.getElementById("root");
