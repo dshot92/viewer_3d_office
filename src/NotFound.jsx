0f2c2fae978782fdcd2632f43d33eee5e3e1886f
dshot92 <dshot92@gmail.com>
2023-03-12 11:32:18 +0100

    Redirect automatically after 10 seconds

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-const NotFound = () => (
-	<>
-		<div style={{ display: "flex", flexDirection: "column", height: "100dvh", alignItems: "center", justifyContent: "center" }}>
-			<h1>404 - Not Found!</h1>
-			<Link to="/">Go Home</Link>
-		</div>
-	</>
-);
+function NotFound() {
+    const navigate = useNavigate();
+
+    // Redirect automatically after 10 seconds
+    setTimeout(() => {
+        console.log("time")
+        navigate("/");
+    }, 10000);
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column", height: "100dvh", alignItems: "center", justifyContent: "center" }}>
+		<h1>404 - Not Found!</h1>
+		<Link to="/">Go Home</Link>
+	</div>
+    )
+};
 
 export default NotFound;
