5ba12daa537aa2b7c8020bcd7f56a545d2845084
dshot92 <dshot92@gmail.com>
2023-03-22 00:16:29 +0100

    spaces to tabs

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 function NotFound() {
-    const navigate = useNavigate();
+		const navigate = useNavigate();
 
-    // Redirect automatically after 10 seconds
-    setTimeout(() => {
-        console.log("time")
-        navigate("/");
-    }, 10000);
+		// Redirect automatically after 10 seconds
+		setTimeout(() => {
+			console.log("time");
+			navigate("/");
+		}, 10000);
 
-    return (
-        <div style={{ display: "flex", flexDirection: "column", height: "100dvh", alignItems: "center", justifyContent: "center" }}>
-		<h1>404 - Not Found!</h1>
-		<Link to="/">Go Home</Link>
-	</div>
-    )
+		return (
+			<div style={{ display: "flex", flexDirection: "column", height: "100dvh", alignItems: "center", justifyContent: "center" }}>
+				<h1>404 - Not Found!</h1>
+				<Link to="/">Go Home</Link>
+			</div>
+		);
 };
 
 export default NotFound;
